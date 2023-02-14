ad7cd5ff447e07a5a2ace8619f482bd9a74b7899
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-02-14 23:42:00 +0000

    criação do template parte-1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+#POSTS MOCK
+posts=[
+    {
+        "titulo":"Post 1",
+        "texto":"Meu primeiro Post"
+    },
+    {
+        "titulo":"Post 2",
+        "texto":"Olha eu aqui de novo"
+    },
+    {
+        "titulo":"Post 3",
+        "texto":"Novo Post"
+    }
+]
+
+@app.route('/')
+def exibir_entradas():
+    return render_template('exibir_entradas.html', entradas=posts)
+
+@app.route("/pudim")
+def pudim():
+    return "<h1>teste pudim</h1>"
