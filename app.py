08c71a76a07636639ac6b85c1ec61e50e178afe2
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-02-15 23:15:32 +0000

    inserir implementado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,session
+from flask import Flask,render_template,request,redirect,url_for,session,flash
 
 app = Flask(__name__)
 SECRET_KEY = "pudim"
@@ -23,16 +23,32 @@ posts=[
 USERNAME = "admin"
 PASSWORD = "admin"
 
+
+
 @app.route('/')
 def exibir_entradas():
     return render_template('exibir_entradas.html', entradas=posts)
 
+@app.route('/inserir', methods=["POST"])
+def inserir_entradas():
+    novo_post = {
+        "titulo":request.form['titulo'],
+        "texto":request.form['texto']
+    }
+    posts.append(novo_post)
+    return redirect(url_for('exibir_entradas'))
 @app.route('/login',methods=["GET","POST"])
 def login():
     erro = ""
     if request.method == "POST":
         if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
             session['logado'] = True
+            flash("Login efetuado com sucesso")
             return redirect(url_for('exibir_entradas'))
         erro = "Ususário ou senha inválidos"
     return render_template('login.html', erro=erro)
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('Logout efetuado com sucesso')
+    return redirect(url_for('exibir_entradas'))
